cb3061297096c206cf0e0f32a6fdcdb6e58e8b7c
Nadia Degtyareva <nadia_degtiareva@rambler.ru>
2019-06-22 09:47:08 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,3 +14,38 @@ fetch('api/example/example.json')
 	.catch((error) => {
 		console.log('error');
 	});
+
+
+const firstPromise = fetch('api/example/example2.json')
+	.then((response) => response.json());
+
+const secondPromise = fetch('api/example/example3.json')
+	.then((response) => response.json());
+
+secondPromise
+	.then(person => {
+		console.log(person.name);
+
+		return firstPromise;
+	})
+	.then(person => {
+		console.log(person.name);
+	})
+	.catch((error) => {
+		console.warn(error);
+	}) ;
+
+
+const scenario = (resolve, reject) => {
+	document.addEventListener('click', resolve);
+
+	setTimeout(reject, 5000)
+};
+
+const clickPromise = new Promise(scenario);
+
+clickPromise
+	.then(
+		() => console.log('success'),
+		() => console.warn('error'),
+	);
